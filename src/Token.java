a42e8d06d010d584aaae77f08903aeb5d7233d1a
nilsmuralles <murallesnils11@gmail.com>
2024-02-19 15:05:44 -0600

    Añadiendo la clase Token

---
+++ src/Token.java
@@ -0,0 +1,55 @@
+package src;
+
+/**
+ * @author Nils Muralles
+ * @date 19/02/2024
+ * @description Clase que modela un Token
+ * @version 1.0
+ */
+
+public class Token {
+    private String value;
+    private String typeValue;
+
+    /**
+     * @Description Constructor de Token
+     */
+    public Token() {
+
+    }
+
+    /**
+     * @Description Obtener el valor del Token
+     * @return Valor del Token
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @Description Modificar el valor del Token
+     * @param value Valor del Token
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @Description Obtener el tipo del valor del Token
+     * @return Tipo del valor del Token
+     */
+    public String getTypeValue() {
+        return typeValue;
+    }
+
+    /**
+     * @Description Modificar el tipo del valor del Token
+     * @param typeValue Tipo del valor del Token
+     */
+    public void setTypeValue(String typeValue) {
+        this.typeValue = typeValue;
+    }
+
+    
+
+}
